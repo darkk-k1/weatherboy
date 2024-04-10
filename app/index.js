5fdea6dcffd7835b9e3b20e5a286babeae572901
dark <mdomagala@darkabo.eu>
2024-04-10 15:13:49 +0200

    v0.0.2

--- app/index.js
+++ app/index.js
@@ -1,16 +1,15 @@
-const https = require('https');
+const { createCanvas } = require("canvas");
+const fs = require("fs");
 
-https.get('', (resp) => {
-  let data = '';
 
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
+const width = 800;
+const height = 480;
 
-  resp.on('end', () => {
-    console.log(JSON.parse(data));
-  });
+const canvas = createCanvas(width, height);
+const context = canvas.getContext("2d");
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+context.fillStyle = "#764abc";
+context.fillRect(0, 0, width, height);
+
+const buffer = canvas.toBuffer("image/png");
+fs.writeFileSync("app/out/img1.png", buffer);
