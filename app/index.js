a9b77c474a57a808b7f192b065d972442289584b
dark <mdomagala@darkabo.eu>
2024-04-10 13:10:50 +0200

    v0.0.1
    First structure

---
+++ app/index.js
@@ -0,0 +1,16 @@
+const https = require('https');
+
+https.get('', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
